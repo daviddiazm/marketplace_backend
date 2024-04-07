7105bcb69904eeac73a7405dc14174b870147671
daviddiazm <davidcol123dm@gmail.com>
2024-04-06 23:57:24 -0500

    par que funcione con otra base de datos

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize('marketplace_db','postgres','root',{
+export const sequelize = new Sequelize('projectsdb','postgres','root',{
   host: 'localhost',
   dialect: 'postgres'
 });
